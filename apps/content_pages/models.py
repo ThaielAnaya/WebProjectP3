e88a12c3cf994ac4c83ab58b87ddca0d0f8138d8
Tajo <112127158+ThaielAnaya@users.noreply.github.com>
2025-05-15 12:35:59 -0300

    Modelo para noticias
    Me olvide de este

--- apps/content_pages/models.py
+++ apps/content_pages/models.py
@@ -13,3 +13,14 @@ class Page(models.Model):
     def __str__(self):
         return self.title
     
+class News(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class NewsImage(models.Model):
+    news = models.ForeignKey(News, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='news_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
